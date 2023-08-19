c9b060d39d0d97ffba4aad9bc751030d24cd39a7
Murilo20H <murilohenrique123correa@gmail.com>
2023-08-19 10:05:00 -0300

    Listas e aulas

--- Aulas/Aula_53_1908/Aula53_1/backend/server.js
+++ Aulas/Aula_53_1908/Aula53_1/backend/server.js
@@ -1,8 +1,32 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const app = express()
 const port = 8070
 const connectionString = "mongodb+srv://Murilo20H:Murilo20H@appdbaula.cxeaimf.mongodb.net/"
+const Pagamento = require('./models/pagamento')
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/listar-pagamentos", async (req, res) => {
+    try {
+        let pagamentos = await Pagamento.find()
+        return res.status(200).json(pagamentos)
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+})
+
+app.post("/cadastrar-pagamento", async (req, res) => {
+    let pagamento = { ...req.body }
+    try {
+        await Pagamento.create(pagamento)
+        return res.status(201).json({ message: "Pagamento criado com sucesso!" })
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+})
 
 mongoose.connect(connectionString, {
     dbName: "DevsSupermarket"
